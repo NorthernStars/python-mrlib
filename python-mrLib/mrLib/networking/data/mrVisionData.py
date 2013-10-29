a070c172d60a011550feed9a5c2c35de9a223775
northernstars <mail@northern-stars.de>
2013-10-29 14:26:29 +0100

    Updated Vision data and szenario Added image to default graphics type.

--- python-mrLib/mrLib/networking/data/mrVisionData.py
+++ python-mrLib/mrLib/networking/data/mrVisionData.py
@@ -24,6 +24,7 @@ VISION_OBJ_LINE = "VISION_OBJ_LINE"
 VISION_OBJ_CIRCLE = "VISION_OBJ_CIRCLE"
 VISION_OBJ_DOT = "VISION_OBJ_DOT"
 VISION_OBJ_TEXT = "VISION_OBJ_TEXT"
+VISION_OBJ_IMG = "VISION_OBJ_IMG"
 
 class mrGraphicsDataPackage():
     '''
@@ -53,47 +54,61 @@ class mrVisionObject():
     id:          Unique ID of object
     name:        Name of object (could be any string)
     location:    Center position (x,y) of object for bot,
-                 rectangle circle and dot or list of points
-                 (x1, y1, x2, y1, ...) for line
-                 For text labels this position is the bottom
-                 left corner of the label!
+                 rectangle circle and dot.
+                 For text labels and images this position is the bottom
+                 left corner of the label/image!
+    points:      List of points [x1, y1, x2, y2, ...] for line
     angle:       View angle of bot object
     radius:      Radius of circle/dot or width of line
     size:        Size (w,h) of rectangle
-    color:       Color of object
+    color:       Color of object (r,g,b,a)
+    textcolor:   Color of text in textlabel [r,g,b,a]
+    text:        Text if label
+    src:         Source of image
     '''
     
     __type = None
     __id = None
     __name = "None"
-    __location = (0,0)      # (x,y)
+    __location = (0,0)          # (x,y)
+    __points = []               # [x1, y1, x2, y2, ...]
     __angle = 0.0
-    __radius = 1.0
-    __size = (1,1)          # (w,h)
-    __color = (1,1,1,1)
+    __radius = 0.001            # 0.0 - 1.0
+    __size = (0.25,0.25)        # (w,h)
+    __color = (1,1,1,1)         # (r,g,b,a)
+    __textcolor = (1,1,1,1)     # [r,g,b,a]
     __text = ""
+    __src = ""
     
-    def __init__( self, objType=None, objID=None, name="None", location=(0,0), angle=0.0, radius=1.0, size=(1,1), color=(1,1,1,1), text="" ):
+    def __init__( self, objType=None, objID=None, name="None", location=(0,0), points=[], angle=0.0,
+                  radius=0.001, size=(0.25,0.25), color=(1,1,1,1), textcolor=[1,1,1,1], text="", src="" ):
         '''
         Constructor
         @param objType: Type of object
         @param objID: ID of object
         @param name: Name of object
         @param location: Tuple of (x,y) coordinates of objects location
+        @param points: List [x1, y1, x2, y2, ...] of points of lines
         @param angle: Angle in degree of objects view direction
         @param radius: Radius of object
         @param size: Tuple of (w,h) width and height of the object
-        @param color: RGBA color of object
+        @param color: RGBA color tuple of object
+        @param textcolor: RGBA color list of text color inside label 
+        @param text: Text for text label
+        @param src: Source of image
         '''        
         self.__type = objType
         self.__id = objID
         self.__name = name
         self.__location = location
+        self.__points = points
         self.__angle = angle
         self.__radius = radius
         self.__size = size
         self.__color = color
+        self.__textcolor = textcolor
         self.__text = text
+        self.__src = src
         
     def getType(self):
         return self.__type
@@ -109,6 +124,9 @@ class mrVisionObject():
     def getLocation(self):
         return self.__location
     
+    def getPoints(self):
+        return self.__points
+    
     def getAngle(self):
         return self.__angle
     
@@ -121,8 +139,14 @@ class mrVisionObject():
     def getColor(self):
         return self.__color
     
+    def getTextColor(self):
+        return self.__textcolor
+    
     def getText(self):
         return str(self.__text)
+    
+    def getSrc(self):
+        return str(self.__src)
     
     
     def setType(self, objType=None):
@@ -139,6 +163,10 @@ class mrVisionObject():
         if type(location) == tuple and len(location) == 2:
             self.__location = location
             
+    def setPoints(self, points=[]):
+        if type(points) == list:
+            self.__points = points
+            
     def setAngle(self, angle=0.0):
         self.__angle = angle
         
@@ -153,8 +181,16 @@ class mrVisionObject():
         if type(color) == tuple and len(color) == 4:
             self.__color = color
             
+    def setTextColor(self, color=[1,1,1,1]):
+        if type(color) == list and len(color) == 4:
+            self.__textcolor = color
+            
     def setText(self, text=""):
         self.__text = str(text)
+        
+    def setSrc(self, src=""):
+        self.__src = str(src)
+        
             
     def __str__(self):
         '''
--- python-mrLib/mrLib/szenarios/AbstractSzenario.py
+++ python-mrLib/mrLib/szenarios/AbstractSzenario.py
@@ -3,15 +3,21 @@ Created on 25.10.2013
 
 @author: northernstars
 '''
+from os.path import isfile
+
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
+from kivy.uix.image import AsyncImage
+
 from kivy.graphics import Color, Rectangle, Line, Ellipse
 from kivy.graphics.context_instructions import PushMatrix, PopMatrix, Rotate
 
 from mrLib.networking.data.mrVisionData import mrVisionObject
-from mrLib.networking.data.mrVisionData import VISION_OBJ_BOT, VISION_OBJ_RECT, VISION_OBJ_LINE, VISION_OBJ_DOT, VISION_OBJ_CIRCLE, VISION_OBJ_TEXT
+from mrLib.networking.data.mrVisionData import VISION_OBJ_BOT, VISION_OBJ_RECT, VISION_OBJ_LINE
+from mrLib.networking.data.mrVisionData import VISION_OBJ_DOT, VISION_OBJ_CIRCLE, VISION_OBJ_TEXT, VISION_OBJ_IMG
 
-class AbstractGraphicsSzenario(Widget):
+class AbstractGraphicsSzenario(FloatLayout):
     '''
     Abstract class for creating a szenario
     1. To create szenario create subclass of this one.
@@ -21,6 +27,40 @@ class AbstractGraphicsSzenario(Widget):
        data and create gui on this class.
     '''
     _guiObjs = {}
+    
+    def cvPos(self, pos=(0,0)):
+        '''
+        Converts relative positions
+        @param pos: Position (x,y)
+        @return: Converted position (x,y)
+        '''
+        return (self.x+pos[0]*self.width, self.y+pos[1]*self.height)
+    
+    def cvSize(self, size=(0,0)):
+        '''
+        Converts relative size
+        @param size: Size (w,h)
+        @return: Converted size (x,y)
+        '''
+        return (size[0]*self.width, size[1]*self.height)
+    
+    def cvPoints(self, points=[]):
+        '''
+        Converts relative points
+        @param points: List [x1, y1, x2, y2, ...] of points
+        @return: Converted list [x1, y1, x2, y2, ...] of pints
+        '''
+        isX = True
+        retPoints = []
+        for p in points:
+            if isX:
+                p = self.x+self.width*p
+                isX = False
+            else:
+                p = self.y+self.height*p
+                isX = True
+            retPoints.append(p)
+        return retPoints
     
     def _updateObj(self, obj=mrVisionObject()):
         '''
@@ -36,12 +76,21 @@ class AbstractGraphicsSzenario(Widget):
         bid = obj.getID()
         name = obj.getName()
         loc = obj.getLocation()
+        points = obj.getPoints()
         angle = obj.getAngle()
         radius = obj.getRadius()
         size = obj.getSize()
         color = obj.getColor()
+        textcolor = obj.getTextColor()
         objname = str(bid)+":"+name
         text = obj.getText()
+        src = obj.getSrc()
+        
+        # convert position and sizes
+        loc = self.cvPos(loc)
+        size = self.cvSize(size)
+        radius = self.cvSize( (radius, radius) )[0]
+        points = self.cvPoints(points)
         
         # get object from widget children or canvas or None if not found
         wObj = self._getObjFromWidget(obj)
@@ -79,7 +128,7 @@ class AbstractGraphicsSzenario(Widget):
                         Ellipse( pos=pos, size=size  )
             
             if typ == VISION_OBJ_RECT:
-                # draw rectangle
+                # draw rectangle                
                 pos = ( loc[0]-size[0]/2, loc[1]-size[1]/2 )
                 if update:
                     # update object
@@ -101,13 +150,9 @@ class AbstractGraphicsSzenario(Widget):
                     with wObj.canvas:
                         Color( color[0], color[1], color[2], color[3] )
                         Rectangle( size=size, pos=pos )
-                        
-                    
-                    
-            
+
             if typ == VISION_OBJ_LINE:
-                #draw line
-                points = loc              
+                #draw line             
                 if update:
                     # update object
                     assert isinstance(wObj, Line)
@@ -127,7 +172,7 @@ class AbstractGraphicsSzenario(Widget):
                     wObj.pos = pos
                     wObj.size = size
                 else:
-                    # draw new object        
+                    # draw new object
                     wObj = Ellipse( pos=pos, size=size  )
             
             if typ == VISION_OBJ_CIRCLE:
@@ -149,20 +194,40 @@ class AbstractGraphicsSzenario(Widget):
                 else:
                     # draw new object
                     padding = 5
-                    wObj = Label( text=text, pos=loc, markup=True )
+                    wObj = Label( text=text, pos=loc, markup=True, color=textcolor )
                     wObj.texture_update()
                     size = wObj.texture.size
                     wObj.size = (size[0]+2*padding, size[1]+2*padding)
                     
-                    with wObj.canvas.before:
+                    with wObj.canvas.before:                        
                         PushMatrix()
-                        Rotate( angle=angle, axis=(0,0,1), origin=(loc[0], loc[1], 0) )                        
-                    with wObj.canvas.after:
+                        Rotate( angle=angle, axis=(0,0,1), origin=(loc[0], loc[1], 0) )  
+                        Color( color[0], color[1], color[2], color[3] )
+                        Rectangle( pos=wObj.pos, size=wObj.size)   
+                                           
+                    with wObj.canvas.after:                        
                         PopMatrix()
-                    
-                    with wObj.canvas.before:
-                        Color(1,1,1,0.1)
-                        Rectangle( pos=wObj.pos, size=wObj.size)
+                        
+            if typ == VISION_OBJ_IMG:
+                # draw image
+                if update:
+                    # update object
+                    assert isinstance(wObj, AsyncImage)
+                    if wObj.source != src:
+                        wObj.source = src
+                        wObj.reload()
+                    wObj.pos = loc
+                    wObj.size = size
+                else:
+                    # draw new object
+                    if isfile(src):
+                        wObj = AsyncImage( source=src, pos=loc, size=size, allow_stretch=True, keep_ratio=False )
+                        
+                        with wObj.canvas.before:
+                            PushMatrix()
+                            Rotate( angle=angle, axis=(0,0,1), origin=(loc[0], loc[1], 0) )                        
+                        with wObj.canvas.after:
+                            PopMatrix()
         
         # check if to add new object
         if not update and wObj != None:    
@@ -203,7 +268,6 @@ class AbstractGraphicsSzenario(Widget):
                 self.canvas.remove(obj)
             obj = None
             
-        self.__drawCounts = 0
         self._guiObjs = {}
             
         
@@ -215,8 +279,6 @@ class AbstractGraphicsSzenario(Widget):
         if type(data) == list:
             # clear drawn objects
             self._clearObjects()
-        
-            self.__drawCounts += 1
                         
             # draw objects
             for obj in data:
